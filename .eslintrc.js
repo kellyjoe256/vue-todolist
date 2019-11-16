f889459b98d559812ef08cb7d224874dcad92f63
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2019-11-16 17:33:39 +0300

    Custom rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,10 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        indent: ['error', 4],
+        'vue/html-indent': ['error', 4],
+        'vue/script-indent': ['error', 4],
+        'arrow-parens': ['error', 'always'],
     },
     parserOptions: {
         parser: 'babel-eslint',
